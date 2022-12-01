0e57e80d1972841c31565f42655fcf32010ccb63
0xzoz <zeroexzoz@gmail.com>
2022-11-30 22:38:24 -0800

    Template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+	import github from '$lib/images/github.svg';
+	import paragraph from '$lib/images/paragraph.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+<pre class="title">scaffold-move-app
+</pre>
+<a class="paragraph grow" href="https://paragraph.xyz/@0xZOZ/set-up-a-move-developer-environment[sui]">
+	<img class="paragraph" src={paragraph} alt="Sui Dev Setup Img" />
+</a>
+
+
+
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	.title {
+		font-size: 4em;
+		font-weight: bold;
+	}
+
+	.paragraph {
+		width:70%;
+		text-align: center;
+		border-radius: 5%;
+	}
+
+	.grow { 
+	transition: all .2s ease-in-out; 
+	}
+
+	.grow:hover { 
+	transform: scale(1.1); 
+	}
+
+
+
+	/* a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 2em;
+		height: 2em;
+	} */
+
+	/* .corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	} */
+
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	
+	<div class="corner">
+		<a href="https://github.com/scaffold-move-app">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: right;
+		justify-content: right;
+		margin: 1%;
+	}
+
+	.corner {
+		width: 5em;
+		height: 5em;
+	}
+
+	.corner a {
+		display: flex;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 3em;
+		height: 3em;
+		object-fit: contain;
+	}
+
+
+
+	a:hover {
+		transform: scale(1.1);
+	}
+</style>
